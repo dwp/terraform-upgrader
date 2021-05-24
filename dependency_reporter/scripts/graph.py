7c9e06fb6b7a84c92d48e76b7f348ae909601d92
MattToczek <matthew.toczek@dmwgroup.co.uk>
2021-05-24 17:37:43 +0100

    pr failures

--- dependency_reporter/scripts/graph.py
+++ dependency_reporter/scripts/graph.py
@@ -5,13 +5,15 @@ from neo4j import GraphDatabase
 
 
 class Graph:
-
     def __init__(self, uri, user, password):
         self.driver = GraphDatabase.driver(uri, auth=(user, password))
 
     def load_repos_dependencies(self, dependencies_file_path):
         with self.driver.session() as session:
-            session.write_transaction(self._create_repos_dependencies_as_vertices_and_edges, dependencies_file_path)
+            session.write_transaction(
+                self._create_repos_dependencies_as_vertices_and_edges,
+                dependencies_file_path,
+            )
 
     @staticmethod
     def _create_repos_dependencies_as_vertices_and_edges(tx, dependencies_file_path):
@@ -23,7 +25,8 @@ class Graph:
                 UNWIND split(row.Dependencies, ":") AS dependency
                 MATCH (child:Repo {{repoName: dependency}})
                 MERGE (parent)-[d:HAS_DEPENDENCY]->(child)
-            """)
+            """
+        )
 
     def query_graph(self, query_name):
         queries = {"circular_dependencies": self._query_circular_dependencies}
@@ -39,11 +42,12 @@ class Graph:
             f""" 
                 MATCH path = (n:Repo)<-[:HAS_DEPENDENCY*]-()
                 RETURN path, size((n)<--()) AS count LIMIT 25
-            """)
+            """
+        )
         for record in response:
             print(record.data())
             # WIP: Trying to understand how to parse and return the data
-             # results.append(record)
+            # results.append(record)
             data = record.data()
             # results.append({data.get("path")[0].get("repoName"): data.get("path")[2].get("repoName")})
         # print(results)
@@ -54,13 +58,15 @@ class Graph:
 
 
 def main():
-    graph = Graph(os.environ.get("NEO4J_HOST", "bolt://localhost:7687"),
-                  os.environ.get("NEO4J_USER", "neo4j"),
-                  os.environ.get("NEO4J_PASSWORD", "neo4j"))
+    graph = Graph(
+        os.environ.get("NEO4J_HOST", "bolt://localhost:7687"),
+        os.environ.get("NEO4J_USER", "neo4j"),
+        os.environ.get("NEO4J_PASSWORD", "neo4j"),
+    )
     graph.load_repos_dependencies("deps.csv")
     graph.query_graph("circular_dependencies")
     graph.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
