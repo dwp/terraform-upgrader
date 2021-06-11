3aeb45a899cf0683b1232aa921870edf600e3714
steveburton4 <Steven.burton@hotmail.co.uk>
2021-06-11 16:20:42 +0100

    TF13 (#6)
    * TF13

--- circular_dependencies_reporter/test/resources/terraform_example.tf
+++ circular_dependencies_reporter/test/resources/terraform_example.tf
@@ -2,22 +2,22 @@ terraform {
   required_version = "{{terraform_12_version}}"
 
   backend "ec2" {
-    bucket         = "{{state_file_bucket}}"
-    key            = "terraform/example/example.tfstate"
-    region         = "{{state_file_region}}"
-    encrypt        = true
+    bucket  = "{{state_file_bucket}}"
+    key     = "terraform/example/example.tfstate"
+    region  = "{{state_file_region}}"
+    encrypt = true
   }
 }
 
 data "terraform_remote_state" "management" {
-  backend = "s3"
+  backend   = "s3"
   workspace = "management"
 
   config = {
-    bucket         = "{{state_file_bucket}}"
-    key            = "terraform/example/management.tfstate"
-    region         = "{{state_file_region}}"
-    encrypt        = true
+    bucket  = "{{state_file_bucket}}"
+    key     = "terraform/example/management.tfstate"
+    region  = "{{state_file_region}}"
+    encrypt = true
   }
 }
 
--- circular_dependencies_reporter/test/resources/terraform_state_block_example.tf
+++ circular_dependencies_reporter/test/resources/terraform_state_block_example.tf
@@ -1,11 +1,11 @@
 data "terraform_remote_state" "management" {
-  backend = "s3"
+  backend   = "s3"
   workspace = "management"
 
   config = {
-    bucket         = "{{state_file_bucket}}"
-    key            = "terraform/example/management.tfstate"
-    region         = "{{state_file_region}}"
-    encrypt        = true
+    bucket  = "{{state_file_bucket}}"
+    key     = "terraform/example/management.tfstate"
+    region  = "{{state_file_region}}"
+    encrypt = true
   }
 }
